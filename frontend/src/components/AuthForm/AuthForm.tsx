b50681120afcb1452ff97cbf90f736994df0f0e4
Ashish Kumar Roy <ashishroy7652@gmail.com>
2025-03-10 02:17:43 +0530

    feat-added-product-popup

--- frontend/src/components/AuthForm/AuthForm.tsx
+++ frontend/src/components/AuthForm/AuthForm.tsx
@@ -1,6 +1,7 @@
 import { useState, ChangeEvent, FormEvent, useEffect } from "react";
 import styles from "./AuthForm.module.css";
 import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuthApi";
 
 interface FormData {
   email: string;
@@ -12,8 +13,10 @@ const AuthForm: React.FC = () => {
   const navigate = useNavigate();
   const [isLogin, setIsLogin] = useState<boolean>(true);
   const [formData, setFormData] = useState<FormData>({ email: "", password: "", confirmPassword: "" });
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+
+
+
+  const { handleAuth, error, loading } = useAuth();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -21,47 +24,15 @@ const AuthForm: React.FC = () => {
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    setError(null);
-    setLoading(true);
-
-    if (!isLogin && formData.password !== formData.confirmPassword) {
-      setError("Passwords do not match!");
-      setLoading(false);
-      return;
-    }
-
-    try {
-      const url = isLogin ? "http://localhost:8000/api/login" : "http://localhost:8000/api/register";
-      const response = await fetch(url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ 'name':'temp',email: formData.email, password: formData.password }),
-      });
-
-      const data = await response.json();
-      console.log(data);
-      setLoading(false);
-
-      if (!response.ok) throw new Error(data.detail || "Something went wrong");
-      //set token in local storage
-      if (data?.access_token) {
-        localStorage.setItem("accessToken", data.access_token);
-        console.log('data.token',data.access_token);
-        navigate("/home");
-      }
-      setIsLogin(true); // Switch to login mode after signup
-    } catch (err) {
-      setLoading(false);
-      setError(err instanceof Error ? err.message : "Something went wrong");
-    }
+    handleAuth(formData, isLogin);
   };
-  useEffect(()=>{
-     // If user is already logged in, redirect to /home
-     console.log('localStorage.getItem(accessToken)',localStorage.getItem('accessToken'));
-    if(localStorage.getItem('accessToken')){
+  useEffect(() => {
+    // If user is already logged in, redirect to /home
+    console.log('localStorage.getItem(accessToken)', localStorage.getItem('accessToken'));
+    if (localStorage.getItem('accessToken')) {
       navigate('/home');
     }
-  },[navigate])
+  }, [navigate])
 
   return (
     <div className={styles.authContainer}>
@@ -90,7 +61,7 @@ const AuthForm: React.FC = () => {
         <button type="submit" disabled={loading}>{loading ? "Processing..." : isLogin ? "Login" : "Signup"}</button>
 
         <p className={styles.toggleText}>
-          {isLogin ? "Don't have an account?" : "Already have an account?"} 
+          {isLogin ? "Don't have an account?" : "Already have an account?"}
           <span onClick={() => setIsLogin(!isLogin)}>{isLogin ? " Signup" : " Login"}</span>
         </p>
       </form>
--- frontend/src/pages/ProductManagement.tsx
+++ frontend/src/pages/ProductManagement.tsx
@@ -4,38 +4,50 @@ import { tableData } from "../components/Table/constants";
 import style from './Prod.module.css';
 import arrowBack from '../assets/icons/arrow_back.svg';
 import DemandForecastChart from "../components/DemandForecastChart/DemandForecastChart";
+import AddProductPopup from "../components/AddProductPopup/AddProductPopup";
+import { Button } from "@mui/material";
+import { useState } from "react";
+import { Add } from "@mui/icons-material";
 
 
 const ProductManagement = () => {
-    return (
-        <div className="p-10">
-          <Navbar/>
-          <div className={style['searchBarContainer']}>
-            <div className={style['back-button']}>
-                <div className={style['back-icon']}>
-                    <img src={arrowBack} alt="back-icon"></img>
-                </div>
-                <h5>Back</h5>
-            </div>
-            <div className={style['vertical-line']}>
 
-            </div>
-            <div className={style['heading-container']}>
-            <h1 className={style['dashboard-heading']}>Create and Manage Product</h1>
-            <div className={style['demand-forecast']}>
-                DemandForecast
-            </div>
-            </div>
-            <div className={style['vertical-line']}></div>
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="p-10">
+      <Navbar />
+      <div className={style['searchBarContainer']}>
+        <div className={style['back-button']}>
+          <div className={style['back-icon']}>
+            <img src={arrowBack} alt="back-icon"></img>
+          </div>
+          <h5>Back</h5>
         </div>
-          <PriceDetailTable data={{rowData:tableData}}/>
-          <div className={style['demand-forecast-container']}>
-          <DemandForecastChart data={{rowData:tableData}}/>
+        <div className={style['vertical-line']}>
+
+        </div>
+        <div className={style['heading-container']}>
+          <h1 className={style['dashboard-heading']}>Create and Manage Product</h1>
+          <div className={style['demand-forecast']}>
+            DemandForecast
           </div>
         </div>
-    
-      );
-  };
-  
-  export default ProductManagement;
-  
+        <div className={style['vertical-line']}></div>
+        <div>
+          <Button startIcon={<Add />} variant="contained" color="primary" onClick={() => setOpen(true)}>
+            Add New Product
+          </Button>
+        </div>
+      </div>
+      <PriceDetailTable data={{ rowData: tableData }} />
+      <div className={style['demand-forecast-container']}>
+        <DemandForecastChart data={{ rowData: tableData }} />
+      </div>
+      <AddProductPopup open={open} onClose={() => setOpen(false)} />
+    </div>
+
+  );
+};
+
+export default ProductManagement;
